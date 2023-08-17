f89b89fdad050a0f2cea0ea4eea2a2e1d3e5a7d2
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2023-08-17 13:57:24 -0300

    feat: add basic script to upload on s3

---
+++ index.js
@@ -0,0 +1,61 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const s3 = require('@aws-sdk/client-s3');
+const { execSync } = require('child_process');
+const { readFileSync, statSync } = require('fs')
+
+(async () => {
+    try {
+        const AWS_SECRET_ACCESS_KEY_ID = core.getInput('AWS_SECRET_ACCESS_KEY');
+        const AWS_ACCESS_KEY_ID = core.getInput('AWS_ACCESS_KEY_ID');
+        const AWS_REGION = core.getInput('AWS_REGION');
+        const AWS_BUCKET_NAME = core.getInput('AWS_BUCKET_NAME');
+    
+        const localPathUpload = core.getInput('local_path_upload');
+        const bucketPathUpload = core.getInput('bucket_path_upload')
+    
+        core.info(`connecting on AWS S3 in region '${AWS_REGION}'`)
+    
+        const s3Client = new s3.S3Client({
+            region: AWS_REGION,
+            credentials: {
+                accessKeyId: AWS_ACCESS_KEY_ID,
+                secretAccessKey: AWS_SECRET_ACCESS_KEY_ID,
+            },
+        })
+    
+        const stat = statSync(`./${localPathUpload}`)
+    
+        const isDirectory = stat.isDirectory()
+    
+        if (isDirectory)
+            execSync(`tar -z ./${localPathUpload}`)
+        else
+            execSync(`gzip ./${localPathUpload}`)
+    
+        const extensionFile = isDirectory ? 'tar.gz' : 'zip'
+    
+        const file = readFileSync(`./${localPathUpload}.${extensionFile}`)
+    
+        const splitPath = localPathUpload.split('/')
+        const name = splitPath.length === 0 ? localPathUpload : splitPath.pop()
+    
+        const command = new s3.PutObjectCommand({
+            Bucket: AWS_BUCKET_NAME,
+            Body: file,
+            Tagging: `Hostname=${hostname()}&Source=github-actions`,
+            ServerSideEncryption: 'AES256',
+            Key: `${bucketPathUpload}/${name}`,
+            ACL: 'public-read',
+            StorageClass: 'STANDARD',
+        })
+    
+        await s3Client.send(command)
+    
+        const type = isDirectory ? 'file' : 'folder'
+    
+        core.info(`upload ${type} with success`)
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})()
